35870db89c3117eb65d4334ca7adf49fca48f4c8
Tami Bar <fire17@gmail.com>
2021-05-25 16:39:28 +0300

    global runs

--- app.py
+++ app.py
@@ -2010,17 +2010,17 @@ MYPORT = 443
 
 # MYPORT = 2000
 
-runs = 0
+flaskRuns = 0
 
 if __name__ == '__main__':
-	
-	print("1111111111111111111",runs)
-	print("1111111111111111111",runs)
-	print("1111111111111111111",runs)
-	print("1111111111111111111",runs)
-	print("1111111111111111111",runs)
-	print("1111111111111111111",runs)
-	runs += 1
+	global flaskRuns
+	print("1111111111111111111",flaskRuns)
+	print("1111111111111111111",flaskRuns)
+	print("1111111111111111111",flaskRuns)
+	print("1111111111111111111",flaskRuns)
+	print("1111111111111111111",flaskRuns)
+	print("1111111111111111111",flaskRuns)
+	flaskRuns += 1
 	flaskRun(master)
 	if not noFlask:
 		print("STARTING APP")
