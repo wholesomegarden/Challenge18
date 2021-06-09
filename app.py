d9941e5bce08f217c9534d4ba1ee124a9d7dc2ac
Tami Bar <fire17@gmail.com>
2021-06-09 13:10:49 +0300

    no jwt enters xapi, no with jwt

--- app.py
+++ app.py
@@ -1975,9 +1975,9 @@ def flaskRunAsync(data):
 
 # Protect a route with jwt_required, which will kick out requests
 # without a valid JWT present.
-# @jwt_required()
 @app.route("/xapi", methods=["POST"])
 @cross_origin(origin='localhost',headers=['Content- Type','Authorization'])
+@jwt_required()
 def protected():
 	# Access the identity of the current user with get_jwt_identity
 	print("XXXXXXXXXAAAAAAAPPPPPPPIIIIII")
